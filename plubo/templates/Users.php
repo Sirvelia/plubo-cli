b7141836378d297649db14f3e7f327b0dd909542
Joan <joan@sirvelia.com>
2025-03-20 20:03:48 +0100

    fix: Add Users.php

---
+++ plubo/templates/Users.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace PluginPlaceholder\Functionality;
+
+class Users
+{
+
+    protected $plugin_name;
+    protected $plugin_version;
+
+    public function __construct($plugin_name, $plugin_version)
+    {
+        $this->plugin_name = $plugin_name;
+        $this->plugin_version = $plugin_version;
+
+        add_action('init', [$this, 'example_function']);
+    }
+
+    public function example_function()
+    {
+        
+    }
+
+}
